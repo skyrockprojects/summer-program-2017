a4a5e0df31271393612c71138bf664d5970ec147
WenYang-Lin <s2631502@gmail.com>
2017-07-05 23:34:44 +0800

    change folder name/ fix pic

---
+++ Arduino/Intro/System_Pushbutton_LED/System_Pushbutton_LED.ino
@@ -0,0 +1,20 @@
+int buttonPin = 2;
+int LED = 3;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (digitalRead(buttonPin) == LOW) {
+    Serial.println("OFF");
+    digitalWrite(LED, LOW);
+  } 
+  else if (digitalRead(buttonPin) == HIGH) {
+    Serial.println("ON");
+    digitalWrite(LED, HIGH);    
+  }
+  delay(100);
+}
